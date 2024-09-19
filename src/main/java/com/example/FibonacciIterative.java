de62d75b8d103e1cae575c83939bf89c945c2ddf
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-09-19 17:50:09 +0200

    FibonacciIterative.java

--- src/main/java/com/example/FibonacciIterative.java
+++ src/main/java/com/example/FibonacciIterative.java
@@ -1,4 +1,24 @@
 package com.example;
 
 public class FibonacciIterative {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int fib = 1;
+        int prevFib = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int temp = fib;
+            fib = fib + prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println("Fibonacci number is: " + fibonacci(n));
+    }
 }
