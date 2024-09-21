aa0e874e05c2c65b22fae54b9ffd46baf79a22f1
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-09-21 20:09:59 +0200

    FibonacciIterative.java

--- src/main/java/com/example/FibonacciIterative.java
+++ src/main/java/com/example/FibonacciIterative.java
@@ -2,19 +2,21 @@ package com.example;
 
 public class FibonacciIterative {
 
+    // Функція для обчислення n-го числа Фібоначчі ітераційно
     public static int fibonacci(int n) {
         if (n <= 1) {
-            return n;
+            return n; // O(1), O(1)
         }
         int fib = 1;
         int prevFib = 0;
 
+        // O(n) - цикл проходить n-1 разів
         for (int i = 2; i <= n; i++) {
             int temp = fib;
-            fib = fib + prevFib;
-            prevFib = temp;
+            fib = fib + prevFib; // Операція O(1)
+            prevFib = temp;      // Операція O(1)
         }
-        return fib;
+        return fib; // O(1)
     }
 
     public static void main(String[] args) {
@@ -22,3 +24,5 @@ public class FibonacciIterative {
         System.out.println("Fibonacci number is: " + fibonacci(n));
     }
 }
+
+
