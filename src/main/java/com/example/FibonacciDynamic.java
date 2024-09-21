86d698c8e2582dc3a51bb589d35ce88541089e21
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-09-21 20:10:47 +0200

    FibonacciDynamic.java

--- src/main/java/com/example/FibonacciDynamic.java
+++ src/main/java/com/example/FibonacciDynamic.java
@@ -2,19 +2,21 @@ package com.example;
 
 public class FibonacciDynamic {
 
+    // Функція для обчислення n-го числа Фібоначчі за принципом динамічного програмування
     public static int fibonacci(int n) {
         if (n <= 1) {
-            return n;
+            return n; // O(1)
         }
 
-        int[] fibArray = new int[n + 1];
+        int[] fibArray = new int[n + 1]; // O(n) - масив
         fibArray[0] = 0;
         fibArray[1] = 1;
 
+        // O(n) - цикл проходить n-1 разів
         for (int i = 2; i <= n; i++) {
-            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2]; // Операція O(1)
         }
-        return fibArray[n];
+        return fibArray[n]; // O(1)
     }
 
     public static void main(String[] args) {
