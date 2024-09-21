a164d66342739acb6bc26f473dd221eed0c8c2a0
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-09-21 20:09:23 +0200

    FibonacciRecursive.java

--- src/main/java/com/example/FibonacciRecursive.java
+++ src/main/java/com/example/FibonacciRecursive.java
@@ -2,10 +2,12 @@ package com.example;
 
 public class FibonacciRecursive {
 
+    // Функція для обчислення n-го числа Фібоначчі рекурсивно
     public static int fibonacci(int n) {
         if (n <= 1) {
-            return n;
+            return n; // O(1)
         }
+        // O(2^n) - через повторювані обчислення
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
@@ -14,3 +16,4 @@ public class FibonacciRecursive {
         System.out.println("Fibonacci number is: " + fibonacci(n));
     }
 }
+
